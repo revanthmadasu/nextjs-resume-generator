f8e3d6f76302c3ca20b1e6a1244bf26259f28521
Revanth Madasu <revanthmadasu913@gmail.com>
2024-01-26 18:51:44 -0600

    fixed next build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   target: 'serverless',
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core'],
+  },
+  webpack: (
+    config, 
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.resolve.alias['puppeteer-core'] = false;
+    return config;
+  }
 }
 
 module.exports = nextConfig
